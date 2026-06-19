4f90b20c34fe650b9c5dc42748f0366f05cdc8e7
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-06-20 00:16:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -204,18 +204,18 @@ function createNarrowFlightCardHTML(data, rowIdx, setIdx) {
             </div>
 
             <div class="grid grid-cols-[1fr,auto,1fr] items-center text-lg font-black text-[#333333] my-1 gap-2">
-              <div class="flex items-end justify-start gap-1 overflow-hidden">
+              <div class="flex items-center justify-start gap-1 overflow-hidden">
                     <span class="text-sm font-bold text-gray-800">${data[rowIdx+2][setIdx+3]}</span>
                     <span class="text-[6px] text-gray-500 text-center mt-0.5">${data[rowIdx+3][setIdx+3] || ''}</span>
               </div>
               
-              <div class="flex items-center justify-center whitespace-nowrap px-2">
-                  <span class="text-xs font-bold text-gray-800">${formatTime(data[rowIdx+1][setIdx+3])}</span>
-                  <span class="text-gray-300 text-sm justify-center mx-1">→</span>
-                  <span class="text-xs font-bold text-gray-800">${formatTime(data[rowIdx+1][setIdx+4])}</span>
-              </div>
+              <div class="grid grid-cols-[1fr,auto,1fr] items-center gap-1 px-2">
+                    <div class="text-xs font-bold text-gray-800 items-center justify-center">${formatTime(data[rowIdx+1][setIdx+3])}</div>
+                    <div class="text-gray-300 text-sm justify-center">→</div>
+                    <div class="flex text-xs font-bold text-gray-800 items-center justify-center">${formatTime(data[rowIdx+1][setIdx+4])}</div>
+                </div>
               
-              <div class="flex items-end justify-end text-right gap-1 overflow-hidden">
+              <div class="flex items-center justify-end text-right gap-1 overflow-hidden">
                     <span class="text-[6px] text-gray-500 text-center">${data[rowIdx+3][setIdx+4] || ''}</span>
                     <span class="text-sm font-bold text-gray-800 text-left justify-start mt-0.5">${data[rowIdx+2][setIdx+4]}</span>
               </div>
