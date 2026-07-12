b1779a3e797f5a11f0b6f18d656ddbfdf1af029b
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-07-12 19:33:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzzF2n4723EcKe6Gup6JblQCNwhN98R0QZivr84VxFqCCR_vWJ2QdD6TkTX6DCzt9wEKA/exec';
+// config.js
+const CONFIG = {
+    // กำหนดไฟล์และ Action ที่ใช้ในแต่ละหน้า
+    ALL_FLIGHTS: {
+        fileId: '1LTuNtQ7ChcHYkdbI63_zPpr2pTH95BnLCs_JB8dNE50',
+        action: 'Flights'
+    },
+    INTER_FLIGHTS: {
+        fileId: '1LTuNtQ7ChcHYkdbI63_zPpr2pTH95BnLCs_JB8dNE50',
+        action: 'Inter'
+    },
+    DEP_FLIGHTS: {
+        fileId: '1LTuNtQ7ChcHYkdbI63_zPpr2pTH95BnLCs_JB8dNE50',
+        action: 'Dep'
+    },
+    RET_FLIGHTS: {
+        fileId: '1LTuNtQ7ChcHYkdbI63_zPpr2pTH95BnLCs_JB8dNE50',
+        action: 'Ret'
+    }
+    // เพิ่มหน้าใหม่ๆ ได้ที่นี่เลย
+};
