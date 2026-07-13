ae6ec3e38d633d39d53417065da2904c3b208f81
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-07-14 01:29:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -103,7 +103,11 @@ async function fetchAndDisplayFlights(type = 'all') {
     container.innerHTML = '<div class="text-center mt-10 text-gray-500">กำลังโหลดข้อมูล...</div>';
 
     try {
-        const response = await fetch(SCRIPT_URL);
+        const { fileId, action } = CONFIG[ALL_FLIGHTS];
+    
+        // แปลง params (เช่น {type: 'intl'}) ให้เป็น query string
+        const queryString = new URLSearchParams({ fileId, action, ...params }).toString();
+        const response = await fetch(`${SCRIPT_URL}?${queryString}`);
         const data = await response.json();
         container.innerHTML = "";
 
