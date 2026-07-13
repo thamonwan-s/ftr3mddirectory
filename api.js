f1c821d0cf4960d4873c9827d96c734978ba3ccf
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-07-14 03:02:21 +0700

    Update api.js

--- api.js
+++ api.js
@@ -5,7 +5,10 @@ async function fetchFlights(pageKey) {
     // แปลง params (เช่น {type: 'intl'}) ให้เป็น query string
     const queryString = new URLSearchParams({ fileId: fileId, function: action }).toString();
     
-    const response = await fetch(`${SCRIPT_URL}/?${queryString}`);
+    const response = await fetch(`${SCRIPT_URL}?${queryString}`,{
+        method: 'GET',
+        redirect: 'follow'
+    });
     
     if (!response.ok) throw new Error('Network response was not ok');
     return await response.json();
