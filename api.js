c5aeddb3b39f37d33fe220ed2790e9e590b83b73
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-07-14 05:47:19 +0700

    Update api.js

--- api.js
+++ api.js
@@ -11,7 +11,6 @@ async function fetchFlights(pageKey) {
         method: 'GET',
     });
     const textData = await response.text();
-    console.log("ข้อมูลที่ได้จาก Server (Raw):", textData); // <--- เช็คใน Console ว่ามันขึ้นเป็น { ... } หรือขึ้นเป็น <html>
     try {
         return JSON.parse(textData);
     } catch (e) {
