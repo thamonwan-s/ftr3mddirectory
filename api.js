b63225866700d4f656ed33c810eb65c51820aa8d
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-07-14 05:43:06 +0700

    Update api.js

--- api.js
+++ api.js
@@ -27,3 +27,51 @@ async function fetchFlights(pageKey) {
     }
     return await response.json();
 }
+
+/**
+ * เปลี่ยนข้อมูล JSON แนวนอนให้เป็น Grid ที่โค้ดเดิมรู้จัก
+ */
+function prepareGridData(rawData, pageKey) {
+    if (pageKey  === 'ALL_FLIGHTS'){
+        const result = {};
+        const headerRow = rawData[0];
+        for (let setIdx=4; setIdx<headerRow.length; setIdx+=9){
+            const year = headerRow[setIdx];
+            const counters = 1;
+            if (!year) continue;
+            if (!result[year]) {
+                result[year] = {};; // เริ่มนับจาก 1 สำหรับแต่ละปี
+            }
+    
+            for (let r = 3; r<rawData.length; r+=4){
+                const row1 = rawData[r];
+                if(!row1[setIdx]) continue;
+                const row2 = rawData[r+1];
+                const row3 = rawData[r+2];
+                const row4 = rawData[r+3];
+    
+                const i = counters++;
+                const flightObj = {};
+    
+                flightObj['date']=row1[setIdx +0];
+                flightObj['dep_ch']=row1[setIdx +3];
+                flightObj['dep_t']=row2[setIdx +3];
+                flightObj['dep_ap']=row3[setIdx +3];
+                flightObj['dep_pl']=row4[setIdx +3];
+                flightObj['arr_ch']=row1[setIdx +4];
+                flightObj['arr_t']=row2[setIdx +4];
+                flightObj['arr_ap']=row3[setIdx +4];
+                flightObj['arr_pl']=row4[setIdx +4];
+                flightObj['name']=row1[setIdx +5];
+                flightObj['desc']=row2[setIdx +5];
+                flightObj['place']=row3[setIdx +5];
+                flightObj['airport']=row4[setIdx +5];
+                flightObj['flight']=row1[setIdx +6];
+                flightObj['airline']=row1[setIdx +7];
+    
+                result[year][i] = flightObj;
+            }
+        }
+        return result;
+    }
+}
