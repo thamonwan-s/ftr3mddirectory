8250f788d15dc21c74db40dfc7a7bfd1f4631a06
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-07-14 08:08:17 +0700

    Update api.js

--- api.js
+++ api.js
@@ -29,7 +29,46 @@ async function fetchFlights(pageKey) {
  */
 function prepareGridData(rawData, pageKey) {
     if (pageKey === 'REC_FLIGHTS'){
-        pageKey = 'ALL_FLIGHTS';
+        const result = {};
+        const setIdx=4;
+        const year = rawData[0][setIdx];
+        let i = 0;
+        if (!year) continue;
+            if (!result[year]) {
+                result[year] = {};; // เริ่มนับจาก 1 สำหรับแต่ละปี
+            }
+    
+            for (let r = 3; r<rawData.length; r+=4){
+                const row1 = rawData[r];
+                if(!row1[setIdx]) continue;
+                const row2 = rawData[r+1];
+                const row3 = rawData[r+2];
+                const row4 = rawData[r+3];
+    
+                i = i+1;
+                const flightObj = {};
+    
+                flightObj['date']=row1[setIdx +0];
+                flightObj['dep_ch']=row1[setIdx +3];
+                flightObj['dep_t']=row2[setIdx +3];
+                flightObj['dep_ap']=row3[setIdx +3];
+                flightObj['dep_pl']=row4[setIdx +3];
+                flightObj['arr_ch']=row1[setIdx +4];
+                flightObj['arr_t']=row2[setIdx +4];
+                flightObj['arr_ap']=row3[setIdx +4];
+                flightObj['arr_pl']=row4[setIdx +4];
+                flightObj['name']=row1[setIdx +5];
+                flightObj['desc']=row2[setIdx +5];
+                flightObj['place']=row3[setIdx +5];
+                flightObj['airport']=row4[setIdx +5];
+                flightObj['flight']=row1[setIdx +6];
+                flightObj['airline']=row1[setIdx +7];
+    
+                result[year][i] = flightObj;
+            }
+        }
+        console.log("ข้อมูลที่ได้จาก Server (Raw):", result);
+        return result;
     }
     if (pageKey  === 'ALL_FLIGHTS'){
         const result = {};
