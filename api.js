bac59cc3b43ec40eac9c07b125ac613a9d55f9ec
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-07-14 11:36:52 +0700

    Update api.js

--- api.js
+++ api.js
@@ -56,8 +56,7 @@ function prepareGridData(rawData, pageKey) {
         flightObj['flight']=row1[4];
         flightObj['airline']=row1[5];
     
-        result[year][i] = flightObj;
-        break;
+        result[year][0] = flightObj;
     
     }
     else if (pageKey  === 'ALL_FLIGHTS'){
